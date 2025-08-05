6a18275785888552c76932b1a4069723502b110c
MohamedKhaledWorking <mohamed.khaled.mohamed.work@gmail.com>
2025-08-05 22:06:21 +0200

    Add: Create initial pages for coaches, games, home, info, and players

---
+++ sport next/my-project/src/app/coaches/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      coaches age
+    </div>
+  )
+}
+
+export default page
---
+++ sport next/my-project/src/app/players/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      players page
+    </div>
+  )
+}
+
+export default page
