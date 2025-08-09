56be031b205a716df76dec902ca6ddd9ced49000
Mostafa Ramadan <mostafamostafaramadan144@gmail.com>
2025-08-10 00:27:15 +0300

    solve calender

--- src/app/component/ui/calendar.tsx
+++ src/app/component/ui/calendar.tsx
@@ -52,8 +52,13 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ orientation, ...props }) => {
+          return orientation === "left" ? (
+            <ChevronLeft className="h-4 w-4" {...props} />
+          ) : (
+            <ChevronRight className="h-4 w-4" {...props} />
+          );
+        },
       }}
       {...props}
     />
